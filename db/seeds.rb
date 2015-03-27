7664a341c4d39a28e5f9a92353a695173ac0079f
Zlodimir <sslupinos@gmail.com>
2015-03-27 19:44:35 +0300

    added initial data in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.exists?(email: "admin@xfile.com") 
+	User.create(email: "admin@xfile.com", password: "123456789", admin: true)
+end
+
+unless User.exists?(email: "user@xfile.com") 
+	User.create(email: "user@xfile.com", password: "123456789")
+end
+
+["Ebala detected", "Chupacabra detected"].each do | name |
+	if not Xfile.exists?(name: name) 
+		x = Xfile.create(name: name, description: "A sample xfile about #{name}")
+	else
+		x = Xfile.where(name: name).first
+	end
+
+	unless Note.exists?(title: "#{name} note")
+		u = User.where(email: "admin@xfile.com").first
+		Note.create(title: "#{name} note", description: "A long note description about #{name}", xfile: x, author: u)
+	end
+end

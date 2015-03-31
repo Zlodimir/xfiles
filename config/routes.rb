797f024119e3442f99b54ec3d56e113b5f754d7a
Zlodimir <sslupinos@gmail.com>
2015-03-31 09:28:19 +0300

    Added admin panel with creating users func

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   namespace :admin do
     root 'base#index'
 
     resources :xfiles, only: [:new, :create, :destroy]
+    resources :users
   end
 
   devise_for :users
